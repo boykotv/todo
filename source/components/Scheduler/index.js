6091dafe75c8fdf879e68fe6a8b95d97c812dcd1
Tatiana Boyko <tanya.titanya@gmail.com>
2018-12-11 20:53:33 +0200

    finish html

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 //Components
 import Task from 'components/Task';
+import  Checkbox  from '../../theme/assets/Checkbox';
 
 // Instruments
 import Styles from './styles.m.css';
@@ -24,11 +25,11 @@ export default class Scheduler extends Component {
         return (
             <section className = { Styles.scheduler }>
                 <main>
+                    
                     <header>
                         <h1>Планировщик задач</h1>
                         <input type="text"/>
                     </header>
-               
 
                     <section>
                         <form>
@@ -37,25 +38,21 @@ export default class Scheduler extends Component {
                         </form>
                         <div>
                             <ul>
-                                { tasksJSX }
+                                <div style = { { position: 'relative' } }>
+                                    { tasksJSX }
+                                </div>
                             </ul>
                         </div>
                     </section>
 
-
                     <footer>
-                        <div>
-                            <svg version="1.1" viewBox="0 0 27 27" style = { {
-                                                                        width: 25,
-                                                                        height: 25,
-                                                                        display: 'block',
-                                                                    } }>
-                                <g>
-                                    <rect fill="#fff" height="25" rx="5" ry="5" stroke="#363636" width="25" x="1" y="1" style = { { strokewidth: 2 } } ></rect>
-                                    <path d="M22.12 6c-3.12 3.16-6.84 6.36-10.23 9.64l-5.42-4.05L4 14.84l6.78 5.08L12.23 21l1.25-1.25C17 16.2 21.29 12.6 25 8.89z" fill="#fff"></path>
-                                </g>
-                            </svg>
-                        </div>
+                        <Checkbox
+                            Block
+                            checked = { false }
+                            className = { Styles.toggleTaskCompletedState }
+                            color1 = '#363636'
+                            color2 = '#FFF'
+                        /> 
                         <span className = { Styles.completeAllTasks }>Все задачи выполнены</span>
                     </footer>
 
